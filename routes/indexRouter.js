fb07b69067eb38cda78fdbeef4d659211bb3fc90
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2024-11-25 08:01:52 -0500

    Fix info routes

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -7,4 +7,8 @@ indexRouter.get("/", (req, res) =>
   res.render("index", { title: "Mini Messageboard", messages: messages })
 );
 
+indexRouter.get("/:id/message", (req, res) =>
+  res.render("info", { id: req.params.id, messages: messages })
+);
+
 module.exports = indexRouter;
